4eb7b4e773be9998d7261cb5e17585cff14d84c8
rpd4g5 <ryanpd423@gmail.com>
2020-04-21 22:30:30 -0500

    update About.jsx component page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,12 +5,34 @@ var About = React.createClass({
         return (
             <div>
                 <h1 className="text-centered">About</h1>
-                <div className="card">
-                    <div className="card-divider">
-                        This is Single Page Web Application built with React
-                    </div>
-                    <div className="card-section">
-                        <a href="https://github.com/ryanpd423/ReactWeather_2pt0">Check me out on Github!</a>
+                <div>
+                    <p>
+                        This is a Weather Application built with React.
+                        <p>
+                            <a href="https://github.com/ryanpd423/ReactWeather_2pt0">Check me out on Github!</a>
+                        </p>
+                    </p>
+                    <div>
+                        <div>
+                            <p>Below is a list of some of the tools I am using:</p>
+                        </div>
+                        <ul>
+                            <li>
+                                <a href="https://openweathermap.org/">Open Weather Map</a> - This is the free API I used to retrieve the weather data.
+                            </li>
+                            <li>
+                                <a href="https://reactjs.org/">React</a> - I am using React by Facebook to build this SPA's Web UI.
+                            </li>
+                            <li>
+                                <a href="https://webpack.js.org/">Webpack</a> - I am using Webpack to bundle my application.
+                            </li>
+                            <li>
+                                <a href="https://babeljs.io/">Babel</a> - I am using Babel to transpile my React JSX down to vanilla JS.
+                            </li>
+                            <li>
+                                <a href="https://get.foundation/sites/docs/">Foundation</a> - I am using Foundation to style the visual Web UI components.
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
